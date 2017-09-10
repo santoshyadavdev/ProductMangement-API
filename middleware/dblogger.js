08da1ff95277c63e8fa9cce1be07baa58ac86de9
Santosh Yadav <santosh.yadav198613@gmail.com>
2017-09-10 22:54:19 +0530

    added header check

--- middleware/dblogger.js
+++ middleware/dblogger.js
@@ -1,11 +1,21 @@
 var dbLogger = function (req, res, next) {
     if (req.method === 'GET') {
-            console.log(req.params);
+        console.log(req.params);
     }
     if (req.method === 'POST') {
         console.log(req.body);
     }
 
-    next();
+    if (req.method === 'OPTIONS') {
+        next();
+    }
+    if (req.headers.token === "testToken") {
+        console.log('valid request');
+        next();
+    } else {
+        console.log('invalid request');
+        res.status(401).send('Invalid Request');
+    }
+
 }
 module.exports = dbLogger;
--- server.js
+++ server.js
@@ -13,8 +13,9 @@ app.use(bodyparser.urlencoded({
 }));
 
 app.use(function (req, res, next) {
+
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin , X-Requested-With,Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'token ,Origin , X-Requested-With,Content-Type, Accept');
     next();
 });
 
