e3465f248e8da1c98c4f02613e6c47137798851e
Santosh Yadav <santosh.yadav198613@gmail.com>
2017-09-11 17:17:10 +0530

    added getbyid and put method

--- api/product.api.js
+++ api/product.api.js
@@ -1,27 +1,5 @@
 module.exports = function (app, db) {
 
-    var products = [{
-            id: 1,
-            name: 'Product1',
-            price: 100,
-            imageUrl: 'http://test.com',
-            createdDate: new Date('11-10-1987')
-        },
-        {
-            id: 2,
-            name: 'Product2',
-            price: 2000,
-            imageUrl: 'http://test.com',
-            createdDate: new Date('11-10-1987')
-        },
-        {
-            id: 3,
-            name: 'Product3',
-            price: 3000,
-            imageUrl: 'http://test.com',
-            createdDate: new Date('11-10-1987')
-        }
-    ];
 
     app.get('/api/product', function (req, res) {
         db.collection('product').find().toArray(function (err, result) {
@@ -29,20 +7,34 @@ module.exports = function (app, db) {
         });
     });
 
-    // app.get('/api/product/:id', function (req, res) {
-    //     console.log(req.params);
-    //     var id = req.params.id;
-    //     var query = {
-    //         'id': req.params.id
-    //     };
-    //     console.log(id);
-    //     var cursor = db.collection('product').find(query);
-    //     // console.log(data);
-    //     cursor.nextObject(function (err, item) {
-    //         console.log(item);
-    //     });
-    //     res.send(id);
-    // });
+    app.get('/api/product/:id', function (req, res) {
+        console.log(req.params);
+        var id = parseInt(req.params.id);
+        db.collection('product').find({
+            id: id
+        }).toArray(function (err, result) {
+            console.log(result);
+            res.send(result);
+        });
+    });
+
+
+    app.put('/api/product', function (req, res) {
+        console.log(req.body);
+        var id = parseInt(req.body.id);
+        db.collection('product').updateOne({
+            id: id
+        }, {
+            $set: {
+                name: req.body.name,
+                imageUrl: req.body.imageUrl,
+                price: parseFloat(req.body.price)
+            }
+        }).then(function (result) {
+            console.log(result);
+            res.send(result);
+        })
+    });
 
     app.post('/api/product', function (req, res) {
         products.push(req.body);
